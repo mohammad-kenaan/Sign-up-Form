37f78aa565337417e194c794babbb27aa91752e2
Mohamad <177649063+mohammad-kenaan@users.noreply.github.com>
2025-06-27 17:24:02 -0400

    feat: add JS logic to validate password confirmation

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const form = document.querySelector("form");
+const pass = document.querySelector("#password");
+const cfPassword = document.querySelector("#confirm-password");
+const cfPasswordSpan= document.querySelector("#cfPass-span");
+
+
+form.addEventListener("submit", (e) => {
+  if(pass.value !== cfPassword.value) {
+    e.preventDefault();
+    cfPassword.focus();
+    cfPasswordSpan.style.color = "red";
+  }
+
+})
